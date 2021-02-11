891594f0a7897d91226bc1cc76ca1a37012304ae
Diego Rocha <dlmrocha87@gmail.com>
2021-02-11 10:52:21 -0300

    Add modus-theme and dired-subtree

--- config.el
+++ config.el
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-palenight)
+;; (setq doom-theme 'doom-palenight)
+(setq doom-theme 'modus-vivendi)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -60,3 +61,5 @@
 ;; Open emacs fullscreen
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(map! :after dired :map dired-mode-map :leader :n "TAB" #'dired-subtree-toggle)
