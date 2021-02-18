053572cac22c41208bc7960b416a2256b87940b5
Diego Rocha <dlmrocha87@gmail.com>
2021-02-18 10:28:54 -0300

    feat: improve backward kill word

--- config.el
+++ config.el
@@ -63,3 +63,17 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (map! :after dired :map dired-mode-map :leader :n "TAB" #'dired-subtree-toggle)
+
+(defun ryanmarcus/backward-kill-word ()
+  "Remove all whitespace if the character behind the cursor is whitespace, otherwise remove a word."
+  (interactive)
+  (if (looking-back "[ \n]")
+      ;; delete horizontal space before us and then check to see if we
+      ;; are looking at a newline
+      (progn (delete-horizontal-space 't)
+             (while (looking-back "[ \n]")
+               (backward-delete-char 1)))
+    ;; otherwise, just do the normal kill word.
+    (backward-kill-word 1)))
+
+(global-set-key [C-backspace] 'ryanmarcus/backward-kill-word)
